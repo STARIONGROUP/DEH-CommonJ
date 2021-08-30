06999cd71736266f4e51368ad4abaa1d2cc08342
nathanael smiechowski <n.smiechowski@rheagroup.com>
2021-08-30 11:12:04 +0200

    Sam's comment

--- src/main/java/ViewModels/HubLoginViewModel.java
+++ src/main/java/ViewModels/HubLoginViewModel.java
@@ -7,12 +7,12 @@
  *
  * This file is part of DEH-CommonJ
  *
- * The DEH-MDSYSML is free software; you can redistribute it and/or
+ * The DEH-CommonJ is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * The DEH-MDSYSML is distributed in the hope that it will be useful,
+ * The DEH-CommonJ is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
